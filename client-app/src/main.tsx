8f7d37d2b549333ebbef7e31cdfa5fbfeb55f57c
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-10-31 23:10:56 +0200

    Add favicon, base font and updates.

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
+import './global-styles/index.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
