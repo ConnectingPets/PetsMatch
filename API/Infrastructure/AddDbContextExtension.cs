f8380f957da08090af0f1b2a9c615ffc223155be
ivaylo-milanov <imilanov24@gmail.com>
2023-11-02 13:48:41 +0200

    Added summary comment to the AddDbContext extension method

--- API/Infrastructure/AddDbContextExtension.cs
+++ API/Infrastructure/AddDbContextExtension.cs
@@ -3,6 +3,10 @@
     using Microsoft.EntityFrameworkCore;
     using Persistence;
 
+    /// <summary>
+    /// This is an extension method for configuration DbContext. 
+    /// It gets the connection string from the configuration and add the db context to the service collection.
+    /// </summary>
     public static class AddDbContextExtension
     {
         public static IServiceCollection ConfigurateDbContext(this IServiceCollection services, IConfiguration configuration)
