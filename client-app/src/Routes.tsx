99e1f4144c439d3d28f0bc602e27c241103263b2
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-11-03 15:54:31 +0200

    Dashboard page created

--- client-app/src/Routes.tsx
+++ client-app/src/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import { DashboardPage } from './pages/DashboardPage/DashboardPage';
+import { DashboardPage } from './pages/DashboardPage/DashboardPage';
 import {LoginRegisterPage} from './pages/LoginRegisterPage/LoginRegisterPage';
 
 import Page404 from './pages/Page404/Page404';
@@ -10,7 +10,7 @@ const AllRoutes: React.FC = () => {
         <Routes>
             {/* <Route path='/' element={<HomePage />} /> */}
             <Route path='login-register' element={<LoginRegisterPage />} />
-            {/* <Route path='dashboard' element={<DashboardPage/>}/> */}
+            <Route path='dashboard' element={<DashboardPage/>}/>
             <Route path='*' element={<Page404 />} />
         </Routes>
     );
---
+++ client-app/src/pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DashboardPage.scss';
+
+interface DashboardPageProps { }
+
+export const DashboardPage:React.FC<DashboardPageProps> = () => {
+  return (
+    <div>DashboardPage</div>
+  )
+}
