a97faff92de783cbfe3ff397e34b958e15498116
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-11-03 15:50:41 +0200

    MobX and Axios installed

--- client-app/src/Routes.tsx
+++ client-app/src/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+// import { DashboardPage } from './pages/DashboardPage/DashboardPage';
 import {LoginRegisterPage} from './pages/LoginRegisterPage/LoginRegisterPage';
 
 import Page404 from './pages/Page404/Page404';
@@ -8,8 +8,9 @@ import Page404 from './pages/Page404/Page404';
 const AllRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route path='/' element={<HomePage />} />
+            {/* <Route path='/' element={<HomePage />} /> */}
             <Route path='login-register' element={<LoginRegisterPage />} />
+            {/* <Route path='dashboard' element={<DashboardPage/>}/> */}
             <Route path='*' element={<Page404 />} />
         </Routes>
     );
