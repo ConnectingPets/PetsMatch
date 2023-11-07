bc36481cef9fb6d53f5559d9b64853fd855465a1
Kalin Stoev <kstoev9316@gmail.com>
2023-11-07 15:53:53 +0000

    PROD-40 - Pipeline check

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
