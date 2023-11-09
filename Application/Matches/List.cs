a1fabe12d5355b3787fb5473e59361668ae9277b
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-11-09 17:38:15 +0200

    Fixed error with Animal class

--- Application/Matches/List.cs
+++ Application/Matches/List.cs
@@ -5,24 +5,25 @@ namespace Application.Matches
 {
     public class List 
     {
-        //CQRS + MediatR pattern Implemented. 
-        //public class Query : IRequest<List<Cat>> {
+       // CQRS + MediatR pattern Implemented.
+        public class Query : IRequest<List<Animal>>
+        {
 
-        //}
+        }
 
-        //TODO: fix the login in the architecture
-        // public class Handler : IRequestHandler<Query, List<Cat>>
-        // {
-        //     private readonly DataContext _ctx;
-        //     public Handler(DataContext ctx)
-        //     {
-        //         _ctx = ctx;
-        //     }
+       // TODO: fix the login in the architecture
+         public class Handler : IRequestHandler<Query, List<Animal>>
+        {
+            private readonly DataContext _ctx;
+            public Handler(DataContext ctx)
+            {
+                _ctx = ctx;
+            }
 
-        //     public async Task<List<Cat>> Handle(Query req, CancellationToken cancellationToken)
-        //     {
-        //         return await _ctx.Cat.ToListAsync();
-        //     }
-        // }
+            public async Task<List<Cat>> Handle(Query req, CancellationToken cancellationToken)
+            {
+                return await _ctx.Cat.ToListAsync();
+            }
+        }
     }
 }
