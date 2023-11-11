ec5ac5e69467ffc2a11e5dbfd5cb6cdc8030879f
ivaylo-milanov <imilanov24@gmail.com>
2023-11-11 16:01:52 +0200

    Added match user command

---
+++ Application/Match/MatchUserCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace Application.Matches
+{
+    using Domain;
+    using MediatR;
+    using Persistence.Repositories;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class MatchUserCommand
+    {
+        public class MatchAnimalsCommand : IRequest<Unit>
+        {
+            public Guid AnimalOneId { get; set; }
+
+            public Guid AnimalTwoId { get; set; }
+        }
+
+        public class MatchAnimalsHandler : IRequestHandler<MatchAnimalsCommand, Unit>
+        {
+            private readonly IRepository repository;
+
+            public MatchAnimalsHandler(IRepository repository)
+            {
+                this.repository = repository;
+            }
+
+            public async Task<Unit> Handle(MatchAnimalsCommand request, CancellationToken cancellationToken)
+            {
+                Match match = new Match
+                {
+                    AnimalOneId = request.AnimalOneId,
+                    AnimalTwoId = request.AnimalTwoId,
+                    MatchOn = DateTime.Now
+                };
+
+                await repository.AddAsync(match);
+                await repository.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
