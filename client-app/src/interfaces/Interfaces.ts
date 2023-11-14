f5a5fa8a406da4a1e9d76e5aa7e11179cde214b8
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-11-14 16:30:55 +0200

    Install Revalidate and add interface Animal.

--- client-app/src/interfaces/Interfaces.ts
+++ client-app/src/interfaces/Interfaces.ts
@@ -10,3 +10,18 @@ export interface IUser {
     address:string,
     city:string
 }
+
+export interface Animal {
+    name: string | undefined,
+    breed: string | undefined,
+    description?: string | undefined,
+    age: string | undefined,
+    birthDate?: string | undefined,
+    isEducated: string | undefined,
+    photo: string | undefined,
+    healthStatus: string | undefined,
+    gender: string | undefined,
+    socialMedia?: string | undefined,
+    weight?: string | undefined,
+    isHavingValidDocuments: string | undefined
+}
