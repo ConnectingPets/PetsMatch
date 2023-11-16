4862670c49ce73e4e60d594ccba22adb19834a35
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2023-11-16 19:25:44 +0200

    Added functionality for adding animals to user.

---
+++ API/Controllers/AnimalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
