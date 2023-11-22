62b64360e528ff4c23b2604da5e63be47b9d358f
Anton Todorov <antontodorov321@gmail.com>
2023-11-22 19:54:48 +0200

    Add animal controller

---
+++ API/Controllers/AnimalController.cs
@@ -0,0 +1,100 @@
+﻿namespace API.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Infrastructure;
+    using Application.DTOs;
+    using static Application.Animal.AddAnimal;
+    using static Application.Animal.AllAnimal;
+    using static Application.Animal.DeleteAnimal;
+    using static Application.Animal.EditAnimal;
+    using static Application.Animal.ShowAnimalToAdd;
+    using static Application.Animal.ShowAnimalToEdit;
+
+    [Authorize]
+    public class AnimalController : BaseApiController
+    {
+        private readonly IMediator mediator;
+
+        public AnimalController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddAnimal([FromBody] AddAnimalDto animalDto)
+        {
+            string ownerId = this.User.GetById();
+
+            AddAnimalCommand command = new AddAnimalCommand()
+            {
+                AnimalDto = animalDto,
+                OwnerId = this.User.GetById()
+            };
+
+            var result = await mediator.Send(command);
+            return new JsonResult(result);
+        }
+
+        [HttpGet("AddAnimal")]
+        public async Task<IActionResult> AddAnimal()
+        {
+            var animal = await mediator.Send(new ShowAnimalToAddQuery());
+            return new JsonResult(animal);
+        }
+
+        [HttpGet("AllAnimals")]
+        public async Task<IActionResult> GetAllAnimals()
+        {
+            string ownerId = this.User.GetById();
+
+            AllAnimalQuery query = new AllAnimalQuery()
+            {
+                OwnerId = ownerId
+            };
+
+            var allAnimals = await mediator.Send(query);
+            return new JsonResult(allAnimals);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal([FromRoute] string id)
+        {
+            DeleteAnimalCommand command = new DeleteAnimalCommand()
+            {
+                AnimalId = id,
+                UserId = this.User.GetById()
+            };
+
+            return new JsonResult(await mediator.Send(command));
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateAnimal([FromBody] EditAnimalDto animalDto, string id)
+        {
+            EditAnimalCommand command = new EditAnimalCommand()
+            {
+                AnimalDto = animalDto,
+                AnimalId = id,
+                UserId = this.User.GetById()
+            };
+
+            return new JsonResult(await mediator.Send(command));
+        }
+
+        [HttpGet("EditAnimal/{id}")]
+        public async Task<IActionResult> EditAnimal([FromRoute] string id)
+        {
+            ShowAnimalToEditQuery query = new ShowAnimalToEditQuery()
+            {
+                AnimalId = id,
+                UserId = this.User.GetById()
+            };
+
+            return new JsonResult(await mediator.Send(query));
+        }
+
+    }
+}
