c9a9a906c88c784eafb6a91e0367d3354494b198
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2023-11-30 21:27:58 +0200

    Load user's pets on Dashboard page.

--- client-app/src/pages/DashboardPage/DashboardPage.tsx
+++ client-app/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,41 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
+
+import themeStore from '../../stores/themeStore';
+import './DashboardPage.scss';
+import userStore from '../../stores/userStore';
+import agent from '../../api/axiosAgent';
+
 import { CPetCard } from '../../components/common/CPetCard/CPetCard';
 import { CAddPetCard } from '../../components/common/CAddPetCard/CAddPetCard';
 import { UserProfile } from '../../components/UserProfile/UserProfile';
 import { CLogo } from '../../components/common/CLogo/CLogo';
 import { CChangeThemeButton } from '../../components/common/CChangeThemeButton/CChangeThemeButton';
-import themeStore from '../../stores/themeStore';
-import './DashboardPage.scss';
-import userStore from '../../stores/userStore';
 
-const pets = [
-    {
-        name: "Eminem",
-        photo: "https://cdn.theatlantic.com/thumbor/vDZCdxF7pRXmZIc5vpB4pFrWHKs=/559x0:2259x1700/1080x1080/media/img/mt/2017/06/shutterstock_319985324/original.jpg"
-    },
-    {
-        name: "Vanilla Ice",
-        photo: "https://highlandcanine.com/wp-content/uploads/2021/01/vizsla-running.jpg"
-    },
-    {
-        name: "2pac",
-        photo: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=11e949fc5d06576bc8b80ec192896753"
-    },
-    {
-        name: "snoop dogg",
-        photo: "https://a-z-animals.com/media/2021/12/Prettiest-_-Cutest-Dogs-header.jpg"
-    },
-    {
-        name: "BIG",
-        photo: "https://i.pinimg.com/originals/0d/08/60/0d0860d917320784369a58a1f01187d3.jpg"
-    },
-]
+interface DashboardPageProps { }
 
-
-interface DashboardPageProps { };
+interface UserAnimals {
+    id: string,
+    name: string,
+    mainPhoto: string
+}
 
 export const DashboardPage: React.FC<DashboardPageProps> = observer(() => {
+    const [pets, setPets] = useState<UserAnimals[]>([]);
+
+    useEffect(() => {
+        agent.apiAnimal.getAllAnimals()
+            .then(res => {
+                setPets(res.data);
+            });
+    }, []);
+
     return (
         <section className={themeStore.isLightTheme ? 'dashboard__wrapper' : 'dashboard__wrapper dashboard__wrapper__dark'}>
 
@@ -48,7 +42,7 @@ export const DashboardPage: React.FC<DashboardPageProps> = observer(() => {
             <article className={themeStore.isLightTheme ?'dashboard__article ' : 'dashboard__article dashboard__article__dark '}>
                 <h3>my pets</h3>
                 <section className='dashboard__pets'>
-                    {pets.map(x => <CPetCard name={x.name} photo={x.photo} key={x.name} />)}
+                    {pets.map(x => <CPetCard name={x.name} photo={x.mainPhoto} key={x.id} />)}
                     <CAddPetCard />
                 </section>
             </article>
@@ -59,5 +53,5 @@ export const DashboardPage: React.FC<DashboardPageProps> = observer(() => {
             </article>
 
         </section>
-    )
-})
+    );
+});
