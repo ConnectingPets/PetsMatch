754ef4e7cf438e4fd09e61021681f681a9875c63
Ivaylo Milanov <imilanov24@gmail.com>
2023-12-15 12:31:50 +0200

    Removed docker compose

--- API/Infrastructure/AddDbContextExtension.cs
+++ API/Infrastructure/AddDbContextExtension.cs
@@ -12,14 +12,10 @@
     {
         public static IServiceCollection ConfigurateDbContext(this IServiceCollection services, IConfiguration configuration)
         {
-            //string connectionString = configuration.GetConnectionString("DefaultConnection") ??
-            //    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            string connectionString = configuration.GetConnectionString("DefaultConnection") ??
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            string? dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-            string? dbName = Environment.GetEnvironmentVariable("DB_NAME");
-            string? dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-
-            string fullConnectionString = $"Data Source={dbHost};Initial Catalog={dbName};User Id=sa;Password={dbPassword};TrustServerCertificate=true;";
+            string fullConnectionString = connectionString + "TrustServerCertificate=true;";
             services.AddDbContext<DataContext>(options => options.UseSqlServer(fullConnectionString));
 
             return services;
