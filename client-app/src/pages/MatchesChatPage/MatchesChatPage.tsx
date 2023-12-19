12d928ab4ed15fa38cb3e62500e608ac2f648640
ivaylo-milanov <imilanov24@gmail.com>
2023-12-19 17:50:49 +0200

    added visualizing of pet's matches

--- client-app/src/pages/MatchesChatPage/MatchesChatPage.tsx
+++ client-app/src/pages/MatchesChatPage/MatchesChatPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CMatchesHeader } from '../../components/common/CMatchesHeader/CMatchesHeader';
 import { CChangeThemeButton } from '../../components/common/CChangeThemeButton/CChangeThemeButton';
 import themeStore from '../../stores/themeStore';
@@ -7,36 +7,31 @@ import { CMatchCard } from '../../components/common/CMatchCard/CMatchCard';
 import { observer } from 'mobx-react';
 import { CShowHideButton } from '../../components/common/CShowHideButton/CShowHideButton';
 import './MatchesChatPage.scss';
+import agent from '../../api/axiosAgent';
+import { useParams } from 'react-router-dom';
 
-const pets = [
-    {
-        name: "Eminem",
-        photo: "https://cdn.theatlantic.com/thumbor/vDZCdxF7pRXmZIc5vpB4pFrWHKs=/559x0:2259x1700/1080x1080/media/img/mt/2017/06/shutterstock_319985324/original.jpg"
-    },
-    {
-        name: "Vanilla Ice",
-        photo: "https://highlandcanine.com/wp-content/uploads/2021/01/vizsla-running.jpg"
-    },
-    {
-        name: "2pac",
-        photo: "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=11e949fc5d06576bc8b80ec192896753"
-    },
-    {
-        name: "snoop dogg",
-        photo: "https://a-z-animals.com/media/2021/12/Prettiest-_-Cutest-Dogs-header.jpg"
-    },
-    {
-        name: "BIG",
-        photo: "https://i.pinimg.com/originals/0d/08/60/0d0860d917320784369a58a1f01187d3.jpg"
-    },
-]
+interface MatchesChatPageProps { }
 
-interface MatchesChatPageProps { };
+interface IMatch {
+    id: string,
+    name: string,
+    photo: string
+}
 
 export const MatchesChatPage: React.FC<MatchesChatPageProps> = observer(() => {
-
     const [matchesOrMessages, setMatchesOrMessages] = useState(true);
     const [shownMatches, setShownMatches] = useState(true);
+    const [matches, setMatches] = useState<IMatch[]>([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        if (id) {
+            agent.apiMatches.animalMatches(id!)
+            .then(res => {
+                setMatches(res.data);
+            })
+        }
+    }, [id])
 
     const matchesOption = () => {
         setMatchesOrMessages(true);
@@ -68,14 +63,14 @@ export const MatchesChatPage: React.FC<MatchesChatPageProps> = observer(() => {
             <section className={shownMatches ? 'matches__page__matches' : 'matches__page__matches matches__page__matches__hidden'}>
                 <CMatchesHeader />
                 <article className={themeStore.isLightTheme ? 'matches__page__matches__links' : 'matches__page__matches__links matches__page__matches__links__dark'}>
-                    <h4 className={matchesOrMessages ? 'matches__messages__option' : ''} onClick={matchesOption}>matches <span>{pets.length}</span></h4>
+                    <h4 className={matchesOrMessages ? 'matches__messages__option' : ''} onClick={matchesOption}>matches <span>{matches.length}</span></h4>
                     <h4 className={!matchesOrMessages ? 'matches__messages__option' : ''} onClick={messagesOption}>messages<span>{3}</span></h4>
                 </article>
 
                 <article className='matches__page__matches__render' >
                     {
                         matchesOrMessages
-                            ? <>{pets.map(x => <CMatchCard name={x.name} photo={x.photo} key={Math.random()} />)}</>
+                            ? <>{matches.map(match => <CMatchCard name={match.name} photo={match.photo} key={match.id} />)}</>
                             : null
                     }
                 </article>
