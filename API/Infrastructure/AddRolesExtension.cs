2888032bb5dcf13a286510665484975fb84efd9a
Anton Todorov <antontodorov321@gmail.com>
2023-12-20 10:50:48 +0200

    Add extension for seeding roles

---
+++ API/Infrastructure/AddRolesExtension.cs
@@ -0,0 +1,37 @@
+﻿namespace API.Infrastructure
+{
+    using Microsoft.AspNetCore.Identity;
+
+    using static Common.GeneralApplicationConstants;
+
+    public static class AddRolesExtension
+    {
+        public static IApplicationBuilder SeedRoles(this IApplicationBuilder app)
+        {
+            using IServiceScope serviceScope =
+                app.ApplicationServices.CreateScope();
+
+            var serviceProvider = serviceScope.ServiceProvider;
+            var roleManager = serviceProvider.
+                GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+            Task.Run(async () =>
+            {
+                string[] roleNames = { MarketplaceRoleName, MarketplaceRoleName};
+
+                foreach (var roleName in roleNames)
+                {
+                    if (!await roleManager.RoleExistsAsync(roleName))
+                    {
+                        var role = new IdentityRole<Guid>(roleName);
+                        await roleManager.CreateAsync(role);
+                    }
+                }
+            })
+                .GetAwaiter()
+                .GetResult();
+
+            return app;
+        }
+    }
+}
