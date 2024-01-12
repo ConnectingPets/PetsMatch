700f5518a8c0ceb368179a8bf0eb70838009c83c
Anton Todorov <antontodorov321@gmail.com>
2024-01-12 08:49:44 +0200

    Add unit test for all animal categories

---
+++ TestProject1/AnimalCategories/AllAnimalCategoriesTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Tests.AnimalCategories
+{
+    using Domain;
+    using MockQueryable.EntityFrameworkCore;
+    using Moq;
+    using Persistence.Repositories;
+    using System.Linq;
+    using static Application.AnimalCategory.AllAnimalCategories;
+
+    [TestFixture]
+    public class AllAnimalCategoriesTests
+    {
+        [Test]
+        public async Task Handle_ShouldReturn_AllAnimalCategiries()
+        {
+            AnimalCategory animalCategory = new AnimalCategory()
+            {
+                Name = "Test",
+            };
+            var queryable = 
+                new List<AnimalCategory> { animalCategory }.AsQueryable();
+            var asyncEnumerable =
+                new TestAsyncEnumerableEfCore<AnimalCategory>(queryable);
+
+            var mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(r => r.AllReadonly<AnimalCategory>())
+                .Returns(asyncEnumerable);
+
+            var handler = 
+                new AllAnimalCategoriesQueryHandler(mockRepository.Object);
+            var result = await handler.
+                Handle(new AllAnimalCategoriesQuery(), CancellationToken.None);
+
+            Assert.IsTrue(result.IsSuccess);
+            Assert.AreEqual(result.Data.Count(), 1);
+            Assert.AreEqual(result.Data.First().Name, "Test");
+
+        }
+    }
+}
