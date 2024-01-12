ef6469bdf3eaa2729e8de22886d030ebebdfdaa0
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-01-12 11:07:43 +0200

    Add EditPetInMarket page and logic for edit pet in adoption / marketplace.

--- client-app/src/api/axiosAgent.ts
+++ client-app/src/api/axiosAgent.ts
@@ -65,7 +65,9 @@ const apiMessages = {
 const apiMarketplace = {
     getAllAnimalsInMarketplace: () => requests.get('/api/marketplace/AllAnimalsForSale'),
     getMyAnimalsForSale: () => requests.get('/api/marketplace/MyAnimalsForSale'),
-    addAnimal: (animalData: FormData) => requests.post('/api/marketplace/Add', animalData, headers.multipart)
+    getAnimalById: (animalId: string) => requests.get(`/api/marketplace/EditAnimal/${animalId}`),
+    addAnimal: (animalData: FormData) => requests.post('/api/marketplace/Add', animalData, headers.multipart),
+    editAnimalById: (animalId: string, animalData: IAnimal) => requests.patch(`/api/marketplace/${animalId}`, animalData, headers.multipart)
 };
 
 const apiAdoption = {
--- client-app/src/utils/constants.ts
+++ client-app/src/utils/constants.ts
@@ -10,6 +10,12 @@ export type HealthStatusEnum = {
     'Dewormed': number
 };
 
+export type AnimalStatus = {
+    'For adoption': number,
+    'For sale': number,
+    'For swiping': number
+}
+
 export const genderEnum: GenderEnum = {
     'Male': 1,
     'Female': 2,
@@ -20,4 +26,10 @@ export const genderEnum: GenderEnum = {
 export const healthStatusEnum: HealthStatusEnum = {
     'Vaccinated': 1,
     'Dewormed': 2
+};
+
+export const animalStatus: AnimalStatus = {
+    'For adoption': 1,
+    'For sale': 2,
+    'For swiping': 3
 };
