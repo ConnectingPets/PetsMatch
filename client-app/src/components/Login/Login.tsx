7c908f9548c4d671272d6b18c060cb431bf4cf70
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-01-17 19:29:02 +0200

    Fix

--- client-app/src/components/Login/Login.tsx
+++ client-app/src/components/Login/Login.tsx
@@ -52,7 +52,8 @@ export const Login: React.FC<LoginProps> = ({
                         City,
                         Address,
                         Education,
-                        Gender
+                        Gender,
+                        Roles: []
                     }, token);
                 }
 
