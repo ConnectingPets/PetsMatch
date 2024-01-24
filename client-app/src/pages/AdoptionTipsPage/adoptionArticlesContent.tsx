a559401b0d3b0963a02ef47fd404af8c5aad9744
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-01-24 12:51:57 +0200

    Create AdoptionArticle component and add first article extended contet.

---
+++ client-app/src/pages/AdoptionTipsPage/adoptionArticlesContent.tsx
@@ -0,0 +1,30 @@
+export const a1ExtendedContent = (
+    <>
+        <p>Find a pet that fits your family and lifestyle. You need to ask yourself (and the people you live with!) how will a dog or cat fit into our life?</p>
+
+        <ul>
+            <li>Are you an outdoor enthusiast and want to take your best furry bud with you?  Find a dog that will be able to take long hikes, sleep in a tent, hang out in the canoe. Sometimes you can train a cat to do these things too, but we don't recommend it as a starting point!</li>
+            <li>Do you live in a small space, like an apartment? A smaller breed of dog or a cat might be easier to live with.</li>
+            <li>Are you away from home all day? Some breeds of dogs have eparation anxiety, especially those that end up in shelters. The last thing you want is to learn from your neighbors that your dogs is whining and barking all day while you are gone.</li>
+            <li>Do you like a lot of quiet time? A cat, who will likely be very independent, might be the perfect new companion.</li>
+        </ul>
+
+        <p>Think about the life you live now and how your new dog or cat will fit into it and what changes you will have to make to accommodate this new family member. Here are a few things to consider when choosing a new pet:</p>
+
+        <ul>
+            <li>Energy Level - of you and the pet</li>
+            <li>Size when fully grown</li>
+            <li>Relating to other animals and children</li>
+            <li>Noise level of the pet</li>
+        </ul>
+
+        <p>Ensure everyone knows who is responsible for the care of your pet.  If it's just you, make sure you are ready to handle everything on your own.</p>
+
+        <ul>
+            <li>Who will feed them?</li>
+            <li>Who will clean-up after them?</li>
+            <li>Who will check in on them during the day?</li>
+            <li>Who will ensure they get lots of daily exercise?</li>
+        </ul>
+    </>
+);
