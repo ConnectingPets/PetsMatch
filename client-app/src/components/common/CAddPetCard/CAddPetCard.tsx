0d7f5a2ab1ac3f66e5e60c56ff0359865adf456d
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-01-28 17:20:08 +0200

    Improve UX for CAddPetCard.

--- client-app/src/components/common/CAddPetCard/CAddPetCard.tsx
+++ client-app/src/components/common/CAddPetCard/CAddPetCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {BsPlusCircleFill} from 'react-icons/bs';
+import { BsPlusCircleFill } from 'react-icons/bs';
 import themeStore from '../../../stores/themeStore';
 import './CAddPetCard.scss';
 
@@ -10,14 +10,14 @@ interface CAddPetCardProps {
 
 export const CAddPetCard: React.FC<CAddPetCardProps> = ({ link }) => {
     return (
-        <article className={themeStore.isLightTheme ?'add__pet__card' : 'add__pet__card add__pet__card__dark'}>
-            <section className={themeStore.isLightTheme ? 'add__pet__card__cover': 'add__pet__card__cover  add__pet__card__cover__dark'}>
-                <Link className={themeStore.isLightTheme ?'add__pet__card__cover__content' : 'add__pet__card__cover__content  add__pet__card__cover__content__dark'} to={link == 'market' ? '/market/add-pet' : '/add-pet'}>
+        <Link to={link == 'market' ? '/market/add-pet' : '/add-pet'} className={themeStore.isLightTheme ? 'add__pet__card' : 'add__pet__card add__pet__card__dark'}>
+            <section className={themeStore.isLightTheme ? 'add__pet__card__cover' : 'add__pet__card__cover  add__pet__card__cover__dark'}>
+                <div className={themeStore.isLightTheme ? 'add__pet__card__cover__content' : 'add__pet__card__cover__content  add__pet__card__cover__content__dark'}>
                     <BsPlusCircleFill />
                     <h3>add pet</h3>
-                </Link>
-                <h4>more pets <br/> more friends </h4>
+                </div>
+                <h4>more pets <br /> more friends </h4>
             </section>
-        </article>
+        </Link>
     );
 };
